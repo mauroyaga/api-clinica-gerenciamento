e9c9f006167edcecf1a580922ae977b31611e73a
Mauro Cesar <mauro.yaga@outlook.com>
2024-10-24 11:32:49 -0300

    inicio-do-projeto-testes-do-servidor

---
+++ src/test/java/com/br/gerenciamento_clinica/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.gerenciamento_clinica.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
